8a9b49fa098fcd5374cee1548902d86409754f9d
Elena295 <elenadavidovich6@gmail.com>
2025-11-26 09:15:16 +0600

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+spl_autoload_register(function ($class) {
+    if (strpos($class, 'Twig\\') === 0) {
+        $file = __DIR__ . '/../vendor/Twig/src/' . str_replace('\\', '/', substr($class, 5)) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+});
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.html.twig');
+?>
